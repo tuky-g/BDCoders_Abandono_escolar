0f4993deca5e012f86dba6ac0b5a5b6da60ad726
Marta G Arias <marta.gonzalez.arias@gmail.com>
2023-05-10 00:29:39 +0200

    despligue app

--- 5_Streamlit/streamlit_fhs.py
+++ 5_Streamlit/streamlit_fhs.py
@@ -47,7 +47,7 @@ def prepara_df(df_pred):
 
 # precargo el modelo
 
-model_riesgo=joblib.load('/app/BDCoders_Abandono_escolar/5_Streamlit/streamlit/models/RiesgoAbandono/knn_st.joblib')
+model_riesgo=joblib.load('/app/BDCoders_Abandono_escolar/5_Streamlit/models/RiesgoAbandono/knn_st.joblib')
 
 
 
@@ -112,9 +112,9 @@ st.write('Has seleccionado:', topic)
 
 # Create and generate a word cloud image:
 def create_wordcloud(topic):
-    word=list(pd.read_csv('/app/BDCoders_Abandono_escolar/5_Streamlit/streamlit/wordclouds/words_'+str(topic)+'.csv')['0'])
+    word=list(pd.read_csv('/app/BDCoders_Abandono_escolar/5_Streamlit/wordclouds/words_'+str(topic)+'.csv')['0'])
 
-    wordcloud = WordCloud(max_font_size=50, max_words=100,font_path="/app/BDCoders_Abandono_escolar/5_Streamlit/streamlit/fonts/arial/arial.ttf", background_color="white").generate(' '.join(word))
+    wordcloud = WordCloud(max_font_size=50, max_words=100,font_path="/app/BDCoders_Abandono_escolar/5_Streamlit/fonts/arial/arial.ttf", background_color="white").generate(' '.join(word))
     return wordcloud
 
 
